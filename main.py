3850735ee05db40ccb79f91a2760b94742c10e64
Ali <65303957+ali-02mk@users.noreply.github.com>
2022-07-19 00:12:24 +0000

    Basic functionality added!

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith("$hello"):
+    await message.channel.send("Hello!")
+
+
+my_secret = os.environ['Token']
+
+client.run(my_secret)
+
